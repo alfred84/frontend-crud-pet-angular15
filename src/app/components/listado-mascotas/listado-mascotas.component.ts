0ed788958fc4a088b942e2d430ebc958af9e9609
alfred84 <39415905+alfred84@users.noreply.github.com>
2023-01-28 07:53:19 -0500

    Initial setup

---
+++ src/app/components/listado-mascotas/listado-mascotas.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-listado-mascotas',
+  templateUrl: './listado-mascotas.component.html',
+  styleUrls: ['./listado-mascotas.component.scss']
+})
+export class ListadoMascotasComponent {
+
+}
