361452359f33c704db312511fe452301c04e1359
alfred84 <39415905+alfred84@users.noreply.github.com>
2023-01-29 08:13:48 -0500

    Separando Material a Modulo Shared

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,23 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+// Modulos
 import { AppRoutingModule } from './app-routing.module';
+import { SharedModule } from './shared/shared.module';
 
 // Componentes
 import { AppComponent } from './app.component';
 import { AgregarEditarMascotaComponent } from './components/agregar-editar-mascota/agregar-editar-mascota.component';
 import { ListadoMascotasComponent } from './components/listado-mascotas/listado-mascotas.component';
 import { VerMascotaComponent } from './components/ver-mascota/ver-mascota.component';
-
-// Angular Material
-import {MatTableModule} from '@angular/material/table';
-import {MatPaginatorModule} from '@angular/material/paginator';
-import {MatSortModule} from '@angular/material/sort';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatInputModule} from '@angular/material/input';
-import {MatIconModule} from '@angular/material/icon';
-import {MatTooltipModule} from '@angular/material/tooltip';
-import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -31,14 +23,7 @@ import {MatButtonModule} from '@angular/material/button';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatTableModule,
-    MatPaginatorModule,
-    MatSortModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatIconModule,
-    MatTooltipModule,
-    MatButtonModule
+    SharedModule    
   ],
   providers: [],
   bootstrap: [AppComponent]
